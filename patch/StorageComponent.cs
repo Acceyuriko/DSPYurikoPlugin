f340e9c39955d9f13b0a83cd2eb58d0398c64afb
Acceyuriko <Acceyuriko@gmail.com>
2022-01-21 21:20:35 +0800

    fix: StorageComponent

--- patch/StorageComponent.cs
+++ patch/StorageComponent.cs
@@ -3,28 +3,33 @@ using System;
 
 namespace DSPYurikoPlugin
 {
-  public class StorageComponentPatch
-  {
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(StorageComponent), "AddItem", new Type[] { typeof(int), typeof(int), typeof(int), typeof(int), typeof(bool) })]
-    public static bool AddItem(
-      ref int __result,
-      StorageComponent __instance,
-      int itemId,
-      int count,
-      int inc,
-      bool useBan,
-      object[] __args
-    )
+    public class StorageComponentPatch
     {
-      if (!useBan || __instance.size != __instance.bans || itemId == 0 || (uint)count <= 0U)
-      {
-        return true;
-      }
-      __args[3] = inc;
-      // GameMain.mainPlayer.SetSandCount(GameMain.mainPlayer.sandCount + 100);
-      __result = 1;
-      return false;
+        [HarmonyPrefix]
+        [HarmonyPatch(
+          typeof(StorageComponent),
+          "AddItem",
+          new Type[] { typeof(int), typeof(int), typeof(int), typeof(int), typeof(bool) },
+          new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Normal }
+        )]
+        public static bool AddItem(
+          ref int __result,
+          StorageComponent __instance,
+          int itemId,
+          int count,
+          int inc,
+          out int remainInc,
+          bool useBan
+        )
+        {
+            remainInc = inc;
+            if (!useBan || __instance.size != __instance.bans || itemId == 0 || (uint)count <= 0U)
+            {
+                return true;
+            }
+            // GameMain.mainPlayer.SetSandCount(GameMain.mainPlayer.sandCount + 100);
+            __result = 1;
+            return false;
+        }
     }
-  }
 }
