7b4d7c07d2f858bb800b9a1425e78503308cba07
Acceyuriko <Acceyuriko@gmail.com>
2022-06-11 19:21:26 +0800

    fix: csproj

--- YurikoPlugin.cs
+++ YurikoPlugin.cs
@@ -3,29 +3,29 @@ using HarmonyLib;
 
 namespace DSPYurikoPlugin
 {
-  [BepInPlugin("cc.acceyuriko.dsp", "YurikoPlugin", "1.0.2")]
-  public class YurikoPlugin : BaseUnityPlugin
-  {
-    public void Start()
+    [BepInPlugin("cc.acceyuriko.dsp", "YurikoPlugin", "1.0.6")]
+    public class YurikoPlugin : BaseUnityPlugin
     {
-      Harmony.CreateAndPatchAll(typeof(AchievementLogicPatch));
-      Harmony.CreateAndPatchAll(typeof(BuildTool_ReformPatch));
-      Harmony.CreateAndPatchAll(typeof(DysonNodePatch));
-      Harmony.CreateAndPatchAll(typeof(FactorySystemPatch));
-      Harmony.CreateAndPatchAll(typeof(GameAbnormalityCheckPatch));
-      Harmony.CreateAndPatchAll(typeof(GameHistoryDataPatch));
-      Harmony.CreateAndPatchAll(typeof(GameMainPatch));
-      Harmony.CreateAndPatchAll(typeof(InserterComponentPatch));
-      Harmony.CreateAndPatchAll(typeof(ItemProtoPatch));
-      Harmony.CreateAndPatchAll(typeof(LabComponentPatch));
-      Harmony.CreateAndPatchAll(typeof(MechaPatch));
-      Harmony.CreateAndPatchAll(typeof(ModelProtoPatch));
-      Harmony.CreateAndPatchAll(typeof(PowerGeneratorComponentPatch));
-      Harmony.CreateAndPatchAll(typeof(RecipeProtoPatch));
-      Harmony.CreateAndPatchAll(typeof(StarGenPatch));
-      Harmony.CreateAndPatchAll(typeof(StationComponentPatch));
-      Harmony.CreateAndPatchAll(typeof(StorageComponentPatch));
-      Harmony.CreateAndPatchAll(typeof(UniverseGenPatch));
+        public void Start()
+        {
+            Harmony.CreateAndPatchAll(typeof(AchievementLogicPatch));
+            Harmony.CreateAndPatchAll(typeof(BuildTool_ReformPatch));
+            Harmony.CreateAndPatchAll(typeof(DysonNodePatch));
+            Harmony.CreateAndPatchAll(typeof(FactorySystemPatch));
+            Harmony.CreateAndPatchAll(typeof(GameAbnormalityCheckPatch));
+            Harmony.CreateAndPatchAll(typeof(GameHistoryDataPatch));
+            Harmony.CreateAndPatchAll(typeof(GameMainPatch));
+            Harmony.CreateAndPatchAll(typeof(InserterComponentPatch));
+            Harmony.CreateAndPatchAll(typeof(ItemProtoPatch));
+            Harmony.CreateAndPatchAll(typeof(LabComponentPatch));
+            Harmony.CreateAndPatchAll(typeof(MechaPatch));
+            Harmony.CreateAndPatchAll(typeof(ModelProtoPatch));
+            Harmony.CreateAndPatchAll(typeof(PowerGeneratorComponentPatch));
+            Harmony.CreateAndPatchAll(typeof(RecipeProtoPatch));
+            Harmony.CreateAndPatchAll(typeof(StarGenPatch));
+            Harmony.CreateAndPatchAll(typeof(StationComponentPatch));
+            Harmony.CreateAndPatchAll(typeof(StorageComponentPatch));
+            Harmony.CreateAndPatchAll(typeof(UniverseGenPatch));
+        }
     }
-  }
 }
